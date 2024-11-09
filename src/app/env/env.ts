6670adabd3efbdfe4552b650e090dfe34b49b07b
Izpo17F <ponce.1714.frp@gmail.com>
2024-11-08 23:53:43 -0500

    featuring: cambio de url

--- src/app/env/env.ts
+++ src/app/env/env.ts
@@ -1,7 +1,7 @@
 //src/app/env/env.ts
 export const
 env  = {
-  baseURL : "https://505c-200-62-226-164.ngrok-free.app/api/",
+  baseURL : "https://e334-2001-1388-24af-fbd0-1cb9-3263-61b0-9a95.ngrok-free.app/api/",
   produccion: false
   
 }
